b8d0b63574924c2aed8106d116751059a67909f6
hongming li <hongmingli1988@gmail.com>
2017-11-12 14:48:47 -0800

    add pre register phone table

---
+++ sms/models/pre_register_phone.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class PreRegisterPhone(models.Model):
+    STATUS_CHOICES = (
+        ('NEW', 'NEW'),
+        ('SENT', 'SENT'),
+        ('DONE', 'DONE'),
+    )
+    phone_text = models.CharField(max_length=200)
+    created_date = models.DateTimeField('date published')
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='NEW')
